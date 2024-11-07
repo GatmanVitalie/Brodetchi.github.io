b1aca9ded41919e694c4af545f73dda71db58978
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-11-07 20:23:16 +0200

    Animatie navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function() {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) { // înlocuiește 50 cu valoarea dorită
+    const navbar = document.querySelector(".navbar_container");
+    if (window.scrollY > 1) { // înlocuiește 50 cu valoarea dorită
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
