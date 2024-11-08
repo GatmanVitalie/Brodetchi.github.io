4b0cd8072d0e16bbea98e3f9dd631ee171fdd8a4
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-11-08 19:47:32 +0200

    Telifon

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,23 @@
-window.addEventListener("scroll", function() {
+// Funcția care gestionează adăugarea sau eliminarea clasei "scrolled"
+function handleScroll() {
     const navbar = document.querySelector(".navbar_container");
-    if (window.scrollY > 1) { // înlocuiește 50 cu valoarea dorită
+    if (window.scrollY > 1) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
+    }
+}
+
+// Atașează evenimentul de scroll doar dacă lățimea ecranului este peste 1000px
+if (window.innerWidth > 1000) {
+    window.addEventListener("scroll", handleScroll);
+}
+
+// Adaugă un eveniment care detectează redimensionarea ferestrei și reatașează evenimentul de scroll când este cazul
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 1000) {
+        window.addEventListener("scroll", handleScroll);
+    } else {
+        window.removeEventListener("scroll", handleScroll);
     }
 });
