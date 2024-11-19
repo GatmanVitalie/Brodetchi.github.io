e809a154592a9a434fd2e693e8dceaeb01fd7e95
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-11-19 22:05:41 +0200

    Navbar nou

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,65 +1,60 @@
-// Funcția care gestionează adăugarea sau eliminarea clasei "scrolled"
-function handleScroll() {
-    const navbar = document.querySelector(".navbar_container");
-    if (window.innerWidth > 1000 && window.scrollY > 1) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-}
+// // Funcția care gestionează adăugarea sau eliminarea clasei "scrolled"
+// function handleScroll() {
+//     const navbar = document.querySelector(".navbar_container");
+//     if (window.innerWidth > 1000 && window.scrollY > 1) {
+//         navbar.classList.add("scrolled");
+//     } else {
+//         navbar.classList.remove("scrolled");
+//     }
+// }
 
-window.addEventListener("scroll", handleScroll);
+// window.addEventListener("scroll", handleScroll);
 
-// Adaugă un eveniment care detectează redimensionarea ferestrei și reatașează evenimentul de scroll când este cazul
-window.addEventListener("resize", function () {
-    if (window.innerWidth > 1000) {
-        window.addEventListener("scroll", handleScroll);
+// // Adaugă un eveniment care detectează redimensionarea ferestrei și reatașează evenimentul de scroll când este cazul
+// window.addEventListener("resize", function () {
+//     if (window.innerWidth > 1000) {
+//         window.addEventListener("scroll", handleScroll);
+//     } else {
+//         window.removeEventListener("scroll", handleScroll);
+//     }
+// });
+
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", () => {
+    navbar.classList.toggle("showInput");
+    if (navbar.classList.contains("showInput")) {
+        searchBox.classList.replace("bx-search", "bx-x");
     } else {
-        window.removeEventListener("scroll", handleScroll);
+        searchBox.classList.replace("bx-x", "bx-search");
     }
 });
 
-
-
-
-function toggleMenu() {
-    const hamburger = document.querySelector('.hamburger');
-    hamburger.classList.toggle('active');
-    if (hamburger.classList.contains('active')) {
-        console.log("Hamburger button activated");
-    }
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+    navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+    navLinks.style.left = "-100%";
 }
 
 
-
-
-
-// Scale nav_phone
-const navbar_container = document.querySelector('.navbar_container');
-const nav_phone = document.querySelector('.nav_phone');
-
-
-function adjustScale() {
-    if (window.innerWidth <= 1000) {
-        var navbarHeight = navbar_container.offsetHeight;
-        var navphoneHeight = nav_phone.offsetHeight;
-
-        // Calculează scaleFactor ca 70% din înălțimea navbarului, raportat la înălțimea butonului nav_phone
-        let scaleFactor = (navbarHeight * 0.7) / navphoneHeight;
-
-        // Aplică transformarea de scalare
-        nav_phone.style.transform = `scale(${scaleFactor})`;
-        console.log("Factor de scalare:", scaleFactor);
-    }
-    else {
-        nav_phone.style.transform = `scale(${0.87})`;
-    }
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+    navLinks.classList.toggle("show1");
 }
-
-
-
-// Apelează funcția la încărcare și la redimensionare
-window.addEventListener('load', adjustScale);
-window.addEventListener('resize', adjustScale);
-
-
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+    navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+    navLinks.classList.toggle("show3");
+}
