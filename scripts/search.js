f596d5ac96984e2e572b184382e81d4fae302a86
GatmanVitalie <gatman.vitalie2019@gmail.com>
2024-12-27 21:11:00 +0200

    Modificat mari navbar(header)

--- scripts/search.js
+++ scripts/search.js
@@ -1,28 +1,3 @@
-function showSearchInput() {
-    // Afișează câmpul de input
-    const input = document.querySelector('.search_input');
-    const searchText = document.querySelector('.search_text');
-    
-    // if (input.style.display === "none") {
-    //     input.style.display = "inline-block";
-    //     searchText.style.display = "none";
-    //     input.focus();  // Focalizează pe câmpul de input
-    // } else {
-    //     input.style.display = "none";
-    //     searchText.style.display = "inline";
-    // }
-}
-
-function clearText() {
-    const searchText = document.querySelector('.search_text');
-    searchText.style.display = "none";
-}
-
-function restoreText() {
-    const input = document.querySelector('.search_input');
-    const searchText = document.querySelector('.search_text');
-    
-    if (input.value === "") {
-        searchText.style.display = "inline";
-    }
-}
+document.querySelector('.search_bar').addEventListener('click', function() {
+    this.querySelector('.search_input').focus();
+});
